807cefccd282366bb026aadff9f4166596794b3f
Ungigdu <Ungigdu@protonmail.com>
2020-05-22 10:06:41 +0800

    rm sh

--- abi.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-rm -rf ~/workspace/go/src/github.com/BASChain/go-bmail-resolver/eth
-
-solc ~/workspace/contract/BASChain/smart_contract/contracts/BasView.sol --abi -o ~/workspace/go/src/github.com/BASChain/go-bmail-resolver/eth
-cd  ~/workspace/go/src/github.com/BASChain/go-bmail-resolver/eth
-
-abigen --abi BasView.abi --pkg eth --type BasView --out BasView.go
